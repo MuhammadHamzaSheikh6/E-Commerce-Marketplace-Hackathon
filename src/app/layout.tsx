ba4c3c35654b3edbb5860bd07d45d3f6b7c53137
MuhammadHamzaSheikh6 <m.hamzashaikh6067@gmail.com>
2024-12-22 18:40:40 +0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Header from "@/components/header";
 import Footer from "@/components/footer";
 import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
       >
         <Header/>
         {children}
+        <SpeedInsights />
         <Analytics />
         <Footer/>
       </body>
