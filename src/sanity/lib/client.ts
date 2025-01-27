060b42d509aaa10fade018f083665feb6d0a3a1b
MuhammadHamzaSheikh6 <m.hamzashaikh6067@gmail.com>
2025-01-27 16:45:27 +0500

    fix

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,9 @@
 import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId, token } from '../env'
+import { apiVersion} from '../env'
 export const client = createClient({
-  projectId,
-  token,
-  dataset,
+  projectId: "u04odjdo",
+  token: process.env.NEXT_PUBLIC_SANITY_TOKAN,
+  dataset: "production",
   apiVersion,
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
