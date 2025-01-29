fbd8163378a8ebf6ec2b1a59ca8d9ac01345b23c
MuhammadHamzaSheikh6 <m.hamzashaikh6067@gmail.com>
2025-01-29 16:05:39 +0500

    update2.3

--- src/app/category/Bedroom/page.tsx
+++ src/app/category/Bedroom/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { IoMdShare } from "react-icons/io";
@@ -9,6 +9,7 @@ import Link from "next/link";
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
 import Head from "next/head";
+import toast, { Toaster } from "react-hot-toast";
 
 interface IProduct {
   _id: string;
@@ -24,6 +25,106 @@ interface IProduct {
 export default function BedroomPage() {
   const [data, setData] = useState<IProduct[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleWishlist = (product: IProduct) => {
+    if (!product) return;
+
+    // Retrieve the wishlist from localStorage
+    const wishlist = JSON.parse(localStorage.getItem("wishlist") || "[]");
+
+    // Check if the product is already in the wishlist
+    const isAlreadyInWishlist = wishlist.some(
+      (item: IProduct) => item._id === product._id
+    );
+
+    if (isAlreadyInWishlist) {
+      toast.success("Product is already in your wishlist!", {
+        position: "top-right",
+        duration: 3000,
+        style: {
+          background: "#f87171",
+          color: "white",
+        },
+      });
+      return;
+    }
+
+    // Add the selected product to the wishlist
+    const updatedWishlist = [...wishlist, product];
+
+    // Save the updated wishlist back to localStorage
+    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
+
+    toast.success("Product added to wishlist!", {
+      position: "top-right",
+      duration: 3000,
+      style: {
+        background: "#B88E2F",
+        color: "white",
+      },
+    });
+  };
+
+  // Function to handle sharing
+  const handleShare = (productId: string) => {
+    const productLink = `${window.location.origin}/product/${productId}`;
+
+    // Check if the Web Share API is supported
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Check out this product!",
+          text: "I found this amazing product and thought you might like it.",
+          url: productLink,
+        })
+        .then(() => {
+          toast.success("Product shared successfully!", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#4ade80",
+              color: "white",
+            },
+          });
+        })
+        .catch((error) => {
+          console.error("Error sharing:", error);
+          toast.error("Failed to share product.", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#f87171",
+              color: "white",
+            },
+          });
+        });
+    } else {
+      // Fallback for browsers that don't support the Web Share API
+      navigator.clipboard
+        .writeText(productLink)
+        .then(() => {
+          toast.success("Link copied to clipboard!", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#4ade80",
+              color: "white",
+            },
+          });
+        })
+        .catch((error) => {
+          console.error("Error copying to clipboard:", error);
+          toast.error("Failed to copy link.", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#f87171",
+              color: "white",
+            },
+          });
+        });
+    }
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -52,10 +153,18 @@ export default function BedroomPage() {
 
   return (
     <>
+      <Toaster />
+
       <Head>
         <title>Bedroom Products - Furniro</title>
-        <meta name="description" content="Discover our exclusive collection of bedroom furniture. High-quality products at affordable prices!" />
-        <meta name="keywords" content="bedroom furniture, affordable furniture, high-quality bedroom products, modern bedroom decor" />
+        <meta
+          name="description"
+          content="Discover our exclusive collection of bedroom furniture. High-quality products at affordable prices!"
+        />
+        <meta
+          name="keywords"
+          content="bedroom furniture, affordable furniture, high-quality bedroom products, modern bedroom decor"
+        />
         <meta name="author" content="Furniro" />
       </Head>
 
@@ -92,50 +201,54 @@ export default function BedroomPage() {
         </div>
 
         {/* Product Section */}
-        <div className="py-12 md:px-5 lg:mx-16">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 md:space-y-0 space-y-6 lg:grid-cols-4 md:gap-6 max-w-7xl mx-auto">
+        <div className="py-12 px-8 lg:px-16">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
             {data.map((item) => (
               <div
                 key={item._id}
-                className="relative w-auto group bg-gray-100 overflow-hidden"
+                className="relative bg-gray-100 rounded overflow-hidden group"
               >
                 <Image
-                  src={urlFor(item.productImage).url()}
+                  src={urlFor(item.productImage).width(1000).height(1000).url()}
                   alt={`Image of ${item.title}`}
-                  width={285}
-                  height={301}
-                  className="object-cover"
+                  width={1000}
+                  height={1000}
+                  loading="lazy"
                 />
+
                 {item.dicountPercentage && (
                   <span
-                    className="absolute top-4 right-4 bg-[#E97171] text-white text-xs font-bold py-2 px-4 rounded-full flex items-center justify-center"
+                    className="absolute top-4 right-4 bg-red-500 text-white text-xs font-bold flex items-center justify-center"
                     style={{
                       width: "40px",
                       height: "40px",
-                      textAlign: "center",
-                      lineHeight: "40px",
+                      borderRadius: "50%",
                     }}
                   >
-                    %
-                    {item.dicountPercentage}
+                    %{item.dicountPercentage}
                   </span>
                 )}
+
                 {item.isNew && (
                   <span
-                    className="absolute top-4 right-4 bg-[#2EC1AC] text-white text-xs font-bold py-2 px-4 rounded-full flex items-center justify-center"
+                    className="absolute top-4 right-4 bg-green-500 text-white text-xs font-bold flex items-center justify-center"
                     style={{
                       width: "40px",
                       height: "40px",
-                      textAlign: "center",
-                      lineHeight: "40px",
+                      borderRadius: "50%",
                     }}
                   >
                     NEW
                   </span>
                 )}
+
                 <div className="p-4">
-                  <h3 className="font-semibold text-lg">{item.title}</h3>
-                  <p className="text-gray-500 text-sm">{item.shortDescription}</p>
+                  <h3 className="font-semibold text-lg truncate">
+                    {item.title}
+                  </h3>
+                  <p className="text-gray-500 text-sm truncate">
+                    {item.shortDescription}
+                  </p>
                   <div className="mt-2 flex items-center space-x-2">
                     <span className="font-bold">Rp {item.price}</span>
                     {item.oldPrice && (
@@ -145,22 +258,31 @@ export default function BedroomPage() {
                     )}
                   </div>
                 </div>
-                <div className="absolute inset-0 bg-gray-800 bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                  <Link href={`/product/${item._id}`}>
-                    <button className="bg-white text-[#B88E2F] px-8 py-2 mb-2 font-medium shadow">
+
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+                  <Link href={`/product/${item._id}`} legacyBehavior>
+                    <a className="bg-white text-yellow-600 px-6 py-2 mb-2 font-medium rounded shadow">
                       View Details
-                    </button>
+                    </a>
                   </Link>
-                  <div className="flex space-x-2">
-                    <button className="flex items-center gap-1 text-white">
+                  <div className="flex space-x-4">
+                    <button
+                      onClick={() => handleShare(item._id)}
+                      className="flex items-center gap-1 hover:text-red-500 text-white"
+                    >
                       <IoMdShare />
                       <span>Share</span>
                     </button>
-                    <button className="flex items-center gap-1 text-white">
-                      <MdCompareArrows />
-                      <span>Compare</span>
-                    </button>
-                    <button className="flex items-center gap-1 text-white">
+                    <a href={`/comparison/${item._id}`}>
+                      <button className="flex items-center gap-1 hover:text-red-500 text-white">
+                        <MdCompareArrows />
+                        <span>Compare</span>
+                      </button>
+                    </a>
+                    <button
+                      onClick={() => handleWishlist(item)}
+                      className="flex items-center gap-1 text-white hover:text-red-500"
+                    >
                       <FaRegHeart />
                       <span>Like</span>
                     </button>
--- src/app/category/Dining/page.tsx
+++ src/app/category/Dining/page.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import React, { useEffect, useState } from "react";
 import { IoMdShare } from "react-icons/io";
@@ -9,6 +9,7 @@ import Link from "next/link";
 import Head from "next/head";
 import { client } from "@/sanity/lib/client";
 import { urlFor } from "@/sanity/lib/image";
+import toast, { Toaster } from "react-hot-toast";
 
 interface IProduct {
   _id: string;
@@ -24,6 +25,106 @@ interface IProduct {
 export default function BedroomPage() {
   const [data, setData] = useState<IProduct[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const handleWishlist = (product: IProduct) => {
+    if (!product) return;
+
+    // Retrieve the wishlist from localStorage
+    const wishlist = JSON.parse(localStorage.getItem("wishlist") || "[]");
+
+    // Check if the product is already in the wishlist
+    const isAlreadyInWishlist = wishlist.some(
+      (item: IProduct) => item._id === product._id
+    );
+
+    if (isAlreadyInWishlist) {
+      toast.success("Product is already in your wishlist!", {
+        position: "top-right",
+        duration: 3000,
+        style: {
+          background: "#f87171",
+          color: "white",
+        },
+      });
+      return;
+    }
+
+    // Add the selected product to the wishlist
+    const updatedWishlist = [...wishlist, product];
+
+    // Save the updated wishlist back to localStorage
+    localStorage.setItem("wishlist", JSON.stringify(updatedWishlist));
+
+    toast.success("Product added to wishlist!", {
+      position: "top-right",
+      duration: 3000,
+      style: {
+        background: "#B88E2F",
+        color: "white",
+      },
+    });
+  };
+
+  // Function to handle sharing
+  const handleShare = (productId: string) => {
+    const productLink = `${window.location.origin}/product/${productId}`;
+
+    // Check if the Web Share API is supported
+    if (navigator.share) {
+      navigator
+        .share({
+          title: "Check out this product!",
+          text: "I found this amazing product and thought you might like it.",
+          url: productLink,
+        })
+        .then(() => {
+          toast.success("Product shared successfully!", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#4ade80",
+              color: "white",
+            },
+          });
+        })
+        .catch((error) => {
+          console.error("Error sharing:", error);
+          toast.error("Failed to share product.", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#f87171",
+              color: "white",
+            },
+          });
+        });
+    } else {
+      // Fallback for browsers that don't support the Web Share API
+      navigator.clipboard
+        .writeText(productLink)
+        .then(() => {
+          toast.success("Link copied to clipboard!", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#4ade80",
+              color: "white",
+            },
+          });
+        })
+        .catch((error) => {
+          console.error("Error copying to clipboard:", error);
+          toast.error("Failed to copy link.", {
+            position: "top-right",
+            duration: 3000,
+            style: {
+              background: "#f87171",
+              color: "white",
+            },
+          });
+        });
+    }
+  };
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -52,6 +153,8 @@ export default function BedroomPage() {
 
   return (
     <div>
+      <Toaster />
+
       {/* SEO Meta Tags */}
       <Head>
         <title>Dining Furniture - Shop Now</title>
@@ -59,9 +162,15 @@ export default function BedroomPage() {
           name="description"
           content="Explore our premium dining furniture collection, featuring modern designs and discounts. Perfect for every home!"
         />
-        <meta name="keywords" content="Dining furniture, premium furniture, home decor, discounts" />
+        <meta
+          name="keywords"
+          content="Dining furniture, premium furniture, home decor, discounts"
+        />
         <meta property="og:title" content="Dining Furniture - Shop Now" />
-        <meta property="og:description" content="Explore our premium dining furniture collection, featuring modern designs and discounts." />
+        <meta
+          property="og:description"
+          content="Explore our premium dining furniture collection, featuring modern designs and discounts."
+        />
         <meta property="og:image" content="/shop/banner11.png" />
         <meta property="og:url" content="https://yourwebsite.com/dining" />
         <meta name="twitter:card" content="summary_large_image" />
@@ -100,35 +209,53 @@ export default function BedroomPage() {
         </div>
       </div>
 
-      {/* Product List */}
-      <div className="py-12 md:px-5 lg:mx-16">
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 md:gap-6 max-w-7xl mx-auto">
+      {/* Product Section */}
+      <div className="py-12 px-8 lg:px-16">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 max-w-7xl mx-auto">
           {data.map((item) => (
             <div
               key={item._id}
-              className="relative w-auto group bg-gray-100 overflow-hidden"
+              className="relative bg-gray-100 rounded overflow-hidden group"
             >
               <Image
-                src={urlFor(item.productImage).url()}
+                src={urlFor(item.productImage).width(1000).height(1000).url()}
                 alt={`Image of ${item.title}`}
-                width={285}
-                height={301}
-                className="w-[285px] h-[301px] object-cover"
+                width={1000}
+                height={1000}
                 loading="lazy"
               />
+
               {item.dicountPercentage && (
-                <span className="absolute top-4 right-4 bg-[#E97171] text-white text-xs font-bold py-2 px-4 rounded-full">
-                  % {item.dicountPercentage}
+                <span
+                  className="absolute top-4 right-4 bg-red-500 text-white text-xs font-bold flex items-center justify-center"
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                  }}
+                >
+                  %{item.dicountPercentage}
                 </span>
               )}
+
               {item.isNew && (
-                <span className="absolute top-4 left-4 bg-[#2EC1AC] text-white text-xs font-bold py-2 px-4 rounded-full">
+                <span
+                  className="absolute top-4 right-4 bg-green-500 text-white text-xs font-bold flex items-center justify-center"
+                  style={{
+                    width: "40px",
+                    height: "40px",
+                    borderRadius: "50%",
+                  }}
+                >
                   NEW
                 </span>
               )}
+
               <div className="p-4">
-                <h3 className="font-semibold text-lg">{item.title}</h3>
-                <p className="text-gray-500 text-sm">{item.shortDescription}</p>
+                <h3 className="font-semibold text-lg truncate">{item.title}</h3>
+                <p className="text-gray-500 text-sm truncate">
+                  {item.shortDescription}
+                </p>
                 <div className="mt-2 flex items-center space-x-2">
                   <span className="font-bold">Rp {item.price}</span>
                   {item.oldPrice && (
@@ -138,22 +265,31 @@ export default function BedroomPage() {
                   )}
                 </div>
               </div>
-              <div className="absolute inset-0 bg-gray-800 bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
-                <Link href={`/product/${item._id}`}>
-                  <button className="bg-white text-[#B88E2F] px-8 py-2 mb-2 font-medium shadow">
+
+              <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+                <Link href={`/product/${item._id}`} legacyBehavior>
+                  <a className="bg-white text-yellow-600 px-6 py-2 mb-2 font-medium rounded shadow">
                     View Details
-                  </button>
+                  </a>
                 </Link>
-                <div className="flex space-x-2">
-                  <button className="flex items-center gap-1 text-white">
+                <div className="flex space-x-4">
+                  <button
+                    onClick={() => handleShare(item._id)}
+                    className="flex items-center gap-1 hover:text-red-500 text-white"
+                  >
                     <IoMdShare />
                     <span>Share</span>
                   </button>
-                  <button className="flex items-center gap-1 text-white">
-                    <MdCompareArrows />
-                    <span>Compare</span>
-                  </button>
-                  <button className="flex items-center gap-1 text-white">
+                  <a href={`/comparison/${item._id}`}>
+                    <button className="flex items-center gap-1 hover:text-red-500 text-white">
+                      <MdCompareArrows />
+                      <span>Compare</span>
+                    </button>
+                  </a>
+                  <button
+                    onClick={() => handleWishlist(item)}
+                    className="flex items-center gap-1 text-white hover:text-red-500"
+                  >
                     <FaRegHeart />
                     <span>Like</span>
                   </button>
@@ -162,14 +298,12 @@ export default function BedroomPage() {
             </div>
           ))}
         </div>
-
-        {/* Show More Button */}
         <div className="mt-8 text-center">
-          <Link href="/shop">
+          <a href="/shop">
             <button className="px-14 py-2 text-[#B88E2F] hover:bg-[#b88f2f90] font-medium rounded border-[#B88E2F] border-2 transition">
               Show More
             </button>
-          </Link>
+          </a>
         </div>
       </div>
     </div>
